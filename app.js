6e2471d15c72ad4c133f59c6c585c3536f123a85
Miguel Angel <mpgigat@gmail.com>
2021-08-10 14:14:01 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//require('dotenv').config();
+//import {} from 'dotenv/config.js'
+import dotenv from 'dotenv'
+//import {} from "dotenv/config.js"
+dotenv.config()
+import {Server} from './models/server.js'
+//const Server=require('./models/server');
+
+const server= new Server();
+
+server.listen();
+
+ 
